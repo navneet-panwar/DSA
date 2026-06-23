ba1c798e9780ba6a08fdc0572a1fa576389f88ce
navneet singh panwar <133517497+navneet-panwar@users.noreply.github.com>
2026-06-23 18:30:57 +0530

    Time: 2 ms (71.98%), Space: 44.6 MB (56.63%) - LeetHub

---
+++ Java/Medium/0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+        List<String> ll = new ArrayList<>();
+
+    
+        Parent(n, 0, 0, "",ll);
+        return ll;
+    }
+
+    public static void Parent(int n, int open,int close,String ans, List<String> ll ) {
+        if( open == n && close == n){
+            ll.add(ans);
+            return;      // although it would've worked without return too because unnecessary calls won't happen since both calls are capped inside "if" conditions
+        }
+        if( open < n ){
+            Parent(n, open+1, close, ans + "(",ll); // increase open by 1 if call is of open
+        }
+
+        if( close < open ){
+            Parent(n, open, close+1, ans + ")",ll); // increase open by 1 if call is of open
+        }
+        // also you could've changed the both calls if block place with each other ans would have come similar but in diff order
+    }
+}
+ 
